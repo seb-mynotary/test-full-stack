8c5df0a70a120be10a862dbacad1fee8a4475f11
seb <sebastien@mynotary.fr>
2025-01-15 18:07:14 +0100

    refactor: step 1 instructions

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+/**
+ * Demande d'un client: "Nous voulons changer le domaine de tous les utilisateurs"
+ *
+ * Descriptif de l'environnement:
+ * - 1 provider externe qui gère les utilisateurs (ex: firebase, auth0...)
+ * - 1 base de données interne qui stocke les utilisateurs
+ *
+ * L'objectif est de mettre à jour le domaine de tous les utilisateurs dans les deux systèmes.
+ *
+ * Setup du test :
+ * - La liste des emails est stockée dans la variable `emails`
+ * - Les appels au provider et a la base de donnée sont simulés avec un délai de 200ms
+ */
+
+const emails = []
+
+function main() {
+
+}
